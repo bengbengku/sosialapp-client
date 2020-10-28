073e0c886db0374909109a67e4ed00813ccfacbf
bengbengku <anggiatbenget70@gmail.com>
2020-10-29 05:37:02 +0700

    Setup awal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello first app</h1>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+          </Switch>
+        </Router>
       </div>
     );
   }
