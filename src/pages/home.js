8a4a5a84de8d67e51e3c630e96b6300f5e7c384a
bengbengku <anggiatbenget70@gmail.com>
2020-10-29 06:47:08 +0700

    ADD: Setup grid dan fetch data menggunakan axios

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,20 @@
+import Grid from "@material-ui/core/Grid";
 import React, { Component } from "react";
 
 class home extends Component {
+  componentDidMount() {
+    
+  }
   render() {
     return (
-      <div>
-        <h1>Homepage halaman</h1>
-      </div>
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          <p>Content here...</p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile here...</p>
+        </Grid>
+      </Grid>
     );
   }
 }
