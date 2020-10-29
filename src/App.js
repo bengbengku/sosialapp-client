86b9563c393a74579f6dc85cfb1cbec170619861
bengbengku <anggiatbenget70@gmail.com>
2020-10-29 17:44:38 +0700

    Fix import MUIprovider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import Navbar from "./components/Navbar";
 import home from "./pages/home";
 import login from "./pages/login";
