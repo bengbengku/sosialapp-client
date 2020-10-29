88baff8acc2b29bce5d16fdd1f355cb62c105545
bengbengku <anggiatbenget70@gmail.com>
2020-10-29 18:50:43 +0700

    ADD: Penerapan form login

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,34 +3,98 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from "prop-types";
 import AppIcon from "../images/safari-hat.png";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography"
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const styles = {
   form: {
-    textAlign: 'center'
+    textAlign: "center",
+  },
+  image: {
+    margin: "20px auto 10px auto",
+  },
+  pageTitle: {
+    margin: "10px auto 10px auto",
+  },
+  textField: {
+    margin: "10px auto 10px auto",
+  },
+  button: {
+    marginTop: 20,
   }
-}
+};
 
 class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      error: {},
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
   render() {
-    const {classes} = this.props;
+    const { classes } = this.props;
     return (
       <Grid container className={classes.form}>
-        <Grid item sm/>
+        <Grid item sm />
         <Grid item sm>
-          <img src={AppIcon} alt="safari-hat"/>
+          <img src={AppIcon} alt="safari-hat" className={classes.image} />
           <Typography variant="h3" className={classes.pageTitle}>
             Login
           </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Login
+            </Button>
+          </form>
         </Grid>
-        <Grid item sm/>
+        <Grid item sm />
       </Grid>
     );
   }
 }
 
 login.propTypes = {
-  classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(login);
