8b286710c93f988b61ea8647e38fbf1d08dea4c7
bengbengku <anggiatbenget70@gmail.com>
2020-10-30 00:25:33 +0700

    Store + devtools redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-
 import userReducer from "./reducers/userReducer";
 import dataReducer from "./reducers/dataReducer";
 import uiReducer from "./reducers/uiReducer";
@@ -15,13 +14,9 @@ const reducers = combineReducers({
   UI: uiReducer,
 });
 
-const store = createStore(
-  reducers,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
+const store = createStore(reducers, initialState, enhancer);
 
 export default store;
