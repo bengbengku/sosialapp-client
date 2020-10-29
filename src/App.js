4164ce1b4852e3205a529fabcba9d29e89a234d8
bengbengku <anggiatbenget70@gmail.com>
2020-10-29 07:21:48 +0700

    ADD: Typography set

--- src/App.js
+++ src/App.js
@@ -11,17 +11,20 @@ import signup from "./pages/signup";
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff'
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff",
     },
     secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
-    }
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    useNextVariants: true,
   },
 });
 
