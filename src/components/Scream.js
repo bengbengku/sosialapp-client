29c997a588612fb507d4e6d61b637a8ef9a98f95
bengbengku <anggiatbenget70@gmail.com>
2020-10-29 17:48:20 +0700

    ADD: Penerapan untuk dayjs di createdAt

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import Link from "react-router-dom/Link";
+import { Link } from "react-router-dom";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import dayjs from 'dayjs';
+import relativeTime from "dayjs/plugin/relativeTime";
 
 const styles = {
   card: {
@@ -22,6 +24,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -51,7 +54,7 @@ class Scream extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
