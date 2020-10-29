bce72d4e8a2bd5043f3226ac23c463ff9f7d6054
bengbengku <anggiatbenget70@gmail.com>
2020-10-29 20:42:24 +0700

    ADD: Refactoring thme ke util folder

--- src/App.js
+++ src/App.js
@@ -1,59 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeObject from './util/theme';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import Navbar from "./components/Navbar";
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
-const theme = createMuiTheme({
-  
-  palette: {
-    primary: {
-      light: "#33c9dc",
-      main: "#00bcd4",
-      dark: "#008394",
-      contrastText: "#fff",
-    },
-    secondary: {
-      light: "#ff6333",
-      main: "#ff3d00",
-      dark: "#b22a00",
-      contrastText: "#fff",
-    },
-  },
-  spreadIt: {
-    typography: {
-      useNextVariants: true,
-    },
-    form: {
-      textAlign: "center",
-    },
-    image: {
-      margin: "20px auto 10px auto",
-    },
-    pageTitle: {
-      margin: "10px auto 10px auto",
-    },
-    textField: {
-      margin: "10px auto 10px auto",
-    },
-    button: {
-      marginTop: 20,
-      position: "relative",
-    },
-    customError: {
-      color: "red",
-      fontSize: "0.8rem",
-      marginTop: 10,
-    },
-    progress: {
-      position: "absolute",
-    },
-  },
-});
+const theme = createMuiTheme(themeObject);
 
 class App extends Component {
   render() {
