69c6872ab120f5b1df5172926edb54ac58a67e17
bengbengku <anggiatbenget70@gmail.com>
2020-10-30 00:56:36 +0700

    Axios added

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,4 +1,5 @@
 import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types";
+import axios from "axios";
 
 export const loginUser = (userData, history) => (dispatch) => {
   dispatch({ type: LOADING_UI });
