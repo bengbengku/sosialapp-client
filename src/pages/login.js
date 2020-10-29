bd7fe1e0e5fe53a12ade318c3bebe1be37f1c71e
bengbengku <anggiatbenget70@gmail.com>
2020-10-29 18:19:17 +0700

    Setup login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import AppIcon from "../images/safari-hat.png";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography"
+
+const styles = {
+  form: {
+    textAlign: 'center'
+  }
+}
 
 class login extends Component {
   render() {
+    const {classes} = this.props;
     return (
-      <div>
-        <h1>Login halaman</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm/>
+        <Grid item sm>
+          <img src={AppIcon} alt="safari-hat"/>
+          <Typography variant="h3" className={classes.pageTitle}>
+            Login
+          </Typography>
+        </Grid>
+        <Grid item sm/>
+      </Grid>
     );
   }
 }
 
-export default login;
+login.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(login);
