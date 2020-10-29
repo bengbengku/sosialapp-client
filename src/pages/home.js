77fa1a5cf6666133c25f90448d7a3a9cf37fda0f
bengbengku <anggiatbenget70@gmail.com>
2020-10-29 07:23:21 +0700

    ADD: Get data screams dari api firebase

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,34 @@
 import Grid from "@material-ui/core/Grid";
+import axios from "axios";
 import React, { Component } from "react";
 
 class home extends Component {
+  state = {
+    screams: null,
+  };
   componentDidMount() {
-    
+    axios
+      .get("/screams")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
   render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => <p>{scream.body}</p>)
+    ) : (
+      <p>Loading...</p>
+    );
     return (
       <Grid container spacing={16}>
         <Grid item sm={8} xs={12}>
-          <p>Content here...</p>
+          {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
           <p>Profile here...</p>
