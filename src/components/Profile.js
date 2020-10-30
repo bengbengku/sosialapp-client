766202041bc3c4f088fca7158cf2169c1440b66b
bengbengku <anggiatbenget70@gmail.com>
2020-10-31 05:14:11 +0700

    ADD: Penerapan mengambil data user untuk profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
-import Button from "@material-ui/core/Button";
 import { connect } from "react-redux";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import MuiLink from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+import LocationOn from "@material-ui/icons/LocationOn";
+import LinkIcon from "@material-ui/icons/Link";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
 
 const styles = {
   paper: {
@@ -55,13 +63,85 @@ const styles = {
 class Profile extends Component {
   render() {
     const {
-      classess,
+      classes,
       user: {
         credentials: { handle, createdAt, imageUrl, bio, website, location },
         loading,
+        authenticated,
       },
     } = this.props;
-    return <div></div>;
+
+    let profileMarkup = !loading ? (
+      authenticated ? (
+        <Paper className={classes.paper}>
+          <div className={classes.profile}>
+            <div className="image-wrapper">
+              <img src={imageUrl} alt="profile" className="profile-image" />
+            </div>
+            <hr />
+            <div className="profile-details">
+              <MuiLink
+                component={Link}
+                to={`/users/${handle}`}
+                color="primary"
+                variant="h5"
+              >
+                @{handle}
+              </MuiLink>
+              <hr />
+              {bio && <Typography variant="body2">{bio}</Typography>}
+              <hr />
+              {location && (
+                <Fragment>
+                  <LocationOn color="primary" />
+                  <span>{location}</span>
+                  <hr />
+                </Fragment>
+              )}
+              {website && (
+                <Fragment>
+                  <LinkIcon color="primary" />
+                  <a href={website} target="_blank" re="noopener noreferrer">
+                    {" "}
+                    {website}
+                  </a>
+                  <hr />
+                </Fragment>
+              )}
+              <CalendarTodayIcon color="primary" />{" "}
+              <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
+            </div>
+          </div>
+        </Paper>
+      ) : (
+        <Paper className={classes.paper}>
+          <Typography variant="body2" align="center">
+            No profile found, please login again
+          </Typography>
+          <div className={classes.buttons}>
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to="/login"
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              component={Link}
+              to="/signup"
+            >
+              Singup
+            </Button>
+          </div>
+        </Paper>
+      )
+    ) : (
+      <p>loading...</p>
+    );
+    return profileMarkup;
   }
 }
 
@@ -71,7 +151,7 @@ const mapStateToProps = (state) => ({
 
 Profile.propTypes = {
   user: PropTypes.object.isRequired,
-  classess: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 export default connect(mapStateToProps)(withStyles(styles)(Profile));
