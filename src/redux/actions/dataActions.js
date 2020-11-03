2acad617ccb11d296e451e0cc6ecdc2df340196f
bengbengku <anggiatbenget70@gmail.com>
2020-11-03 23:49:47 +0700

    ADD: Hanya menambahkan fungsi clearErrors

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -80,3 +80,7 @@ export const deleteScream = (screamId) => (dispatch) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const clearErrors = () => (dispatch) => {
+  dispatch({ type: CLEAR_ERRORS });
+};
